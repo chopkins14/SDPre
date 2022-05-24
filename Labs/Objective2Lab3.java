519363c27b98ab70e9b6bddf34b24f72667cfa93
Casey Hopkins <cjhopki@gmail.com>
2022-05-24 15:39:32 -0600

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double average;
+    double num1 = 15;
+    double num2 = 6;
+    double num3 = 2;
+    double sum = 0;
+
+    sum = num1 + num2 + num3;
+    average = sum/3;
+
+    System.out.println("Average: " + average );
+  }
+}
