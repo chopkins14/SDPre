f7681437e9409b3b6eb37d083dbe4562c94f2e9e
Casey Hopkins <cjhopki@gmail.com>
2022-05-24 15:16:53 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Casey Hopkins";
+    int age = 28;
+    String state = "Chicago";
+
+      System.out.println(name);
+      System.out.println(age);
+      System.out.println(state);
+  }
+}
