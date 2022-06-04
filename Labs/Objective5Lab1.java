f711a937cc8e6bf146c4532947cab8ae90dc7926
Casey Hopkins <cjhopki@gmail.com>
2022-06-04 14:39:23 -0600

    complete objective5lab1

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Objective5Lab1 {
+
+  public static void main(String[] args) {
+
+    java.util.Scanner scanner = new java.util.Scanner(System.in);
+    Scanner keyboard = new Scanner(System.in);
+    int num1 = 5;
+    int num2 = 15;
+
+    if(num1 > num2) {
+      System.out.println(+ num1 + " is greater than " + num2 + ".");
+    }
+    else if(num2 > num1) {
+      System.out.println(+ num2 + " is greater than " + num1 + ".");
+  scanner.close();
+    }
+  }
+}
