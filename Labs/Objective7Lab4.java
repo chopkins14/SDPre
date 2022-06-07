65e845279cb9bdf22e48fdb9979d1deb74747045
Casey Hopkins <cjhopki@gmail.com>
2022-06-06 22:54:29 -0600

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+
+    int sum = 0, counter = 20, i = 1;
+
+    while(i <= counter)
+    {
+        sum += i;
+        i++;
+    }
+        System.out.println(sum);
+    }
+  }
