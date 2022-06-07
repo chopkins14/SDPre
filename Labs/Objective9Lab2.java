685a0ea07a015ebe35f909af763c22220c350d51
Casey Hopkins <cjhopki@gmail.com>
2022-06-07 09:43:29 -0600

    complete objective9lab2

---
+++ Labs/Objective9Lab2.java
@@ -0,0 +1,14 @@
+public class Objective9Lab2 {
+  public static void main(String[] args) {
+    printMain();
+    printMethod();
+    printMain();
+  }
+
+  public static void printMain() {
+    System.out.println("In main");
+  }
+  public static void printMethod() {
+    System.out.println("In method");
+  }
+}
