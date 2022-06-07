78d65c86287d16c653dfd3f7ab8bfd8d02c39d92
Casey Hopkins <cjhopki@gmail.com>
2022-06-07 00:22:09 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for (int i=1; i<11; i+=1){
+   System.out.println(i);}
+    for (int h=10; h>0; h-=1){
+   System.out.println(h);}
+  }
+
+    //TODO Write another for loop that will print the numbers 10-1.
+
+
+  }
